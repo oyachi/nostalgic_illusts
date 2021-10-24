94ab53484c6b94c24e5a5c5c370450282236561c
Daichi <ikakarasu714@gmail.com>
2021-10-24 17:27:22 +0900

    [change]appbarのバックアローアイコンの変更

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -8,19 +8,17 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFF5F6F9),
-      appBar: AppBar(
-        backgroundColor: Color(0xFFF5F6F9),
-        leading: SizedBox(
-          width: double.infinity,
-          child: FlatButton(
-            padding: EdgeInsets.zero,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-            color: Colors.white,
-            onPressed: ()=>Navigator.pop(context),
-            child: Icon(Icons.arrow_back_ios),
+        appBar: AppBar(
+          backgroundColor: Color(0xFFF5F6F9),
+          leading: SizedBox(
+            width: double.infinity,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: ()=>Navigator.pop(context),
+              color: Colors.black45,
+            ),
           ),
         ),
-      ),
       body: Column(
         children: [
           IllustPreviwe(),
