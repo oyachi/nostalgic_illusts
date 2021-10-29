683d4be6918c979fd69518b26d13fe3d228b1369
Daichi <ikakarasu714@gmail.com>
2021-10-29 23:33:51 +0900

    [update]詳細画面の実装

--- lib/components/illust_description.dart
+++ lib/components/illust_description.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class IllustDescription extends StatelessWidget {
+  String _title = "";
+  String _description = "";
+  String _reflection = "";
+  IllustDescription(this._title, this._description, this._reflection);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-              margin: EdgeInsets.only(top: 20),
+              margin: const EdgeInsets.only(top: 20),
               width: double.infinity,
-              height: 300,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -14,9 +18,60 @@ class IllustDescription extends StatelessWidget {
                   topRight: Radius.circular(40),
                 ),
                ),
-              child: Text(
-                "Macbook",
-                style: Theme.of(context).textTheme.headline6,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 8.0),
+                        width: 150,
+                        height: 7,
+                        decoration: BoxDecoration(
+                          color: Colors.red[50],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0, bottom: 10.0, left: 20.0, right: 20.0),
+                    child: Text(
+                      _title,
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      _description,
+                      style: TextStyle(
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0, bottom: 10.0, left: 20.0, right: 20.0),
+                    child: Text(
+                      "Reflection",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      _reflection,
+                      style: TextStyle(
+                        color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                ],
               ),
             );
     }
--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 /* components */
 import '../components/illust_preview.dart';
 import '../components/illust_description.dart';
+import '../components/detail_bottom_buttons.dart';
+/* models */
+import '../models/post.dart';
 
 class DetailScreen extends StatelessWidget {
+  Post post;
+  DetailScreen(this.post);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,11 +25,26 @@ class DetailScreen extends StatelessWidget {
             ),
           ),
         ),
-      body: Column(
-        children: [
-          IllustPreviwe(),
-          IllustDescription(),
-        ],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: constraints.maxHeight,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  children: [
+                    IllustPreviwe(post.image!),
+                    IllustDescription(post.title!, post.description!, post.reflection!),
+                    Expanded(child: Container(color: Colors.white,)),
+                    DetailBottomButtons(),
+                  ],
+                ),
+              ),
+            ),
+          );
+        }
       )
     );
   }
