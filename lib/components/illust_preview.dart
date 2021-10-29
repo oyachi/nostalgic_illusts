b0f5a519370b0145d6a83aba498ec56c8ff6a421
Daichi <ikakarasu714@gmail.com>
2021-10-29 23:34:32 +0900

    [update]詳細画面の実装

--- lib/components/illust_preview.dart
+++ lib/components/illust_preview.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class IllustPreviwe extends StatelessWidget {
+  File image;
+  IllustPreviwe(this.image);
   @override
   Widget build(BuildContext context) {
-    return Image.asset("assets/images/macbook1.jpg");
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0, bottom: 0.0, left: 8.0, right: 8.0),
+      child: Image.file(image),
+    );
   }
 }
